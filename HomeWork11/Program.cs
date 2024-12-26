d84ffea91fbe97076bb062164e40abd3831392b3
belan <smakki.92@gmail.com>
2024-12-26 12:59:33 +0300

    Добавьте файлы проекта.

---
+++ HomeWork11/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+
+namespace HomeWork11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var basePath = Utils.GetBasePath();
+            string[] directoryNames = { "TestDir1", "TestDir2" };
+            foreach (var dirName in directoryNames)
+            {
+                var dirPath = Path.Combine(basePath, dirName);
+                DirectoryInfo dir = new DirectoryInfo(dirPath);
+                if (!dir.Exists)
+                {
+                    dir.Create();
+                }
+
+                for (int i = 0; i < 10; i++)
+                {
+                    var fileName = "File" + (i + 1);
+                    var filePath = Path.Combine(dirPath, fileName);
+                    if (!File.Exists(filePath))
+                    {
+                        using (var fs = File.Create(filePath))
+                        {
+                            fs.Write(Encoding.UTF8.GetBytes(fileName+" "));
+                        }
+                        if (Utils.CanWriteToFile(filePath))
+                        {
+                            File.AppendAllTextAsync(filePath, DateTime.Now.ToString());
+                        }
+                    }
+
+                }
+            }
+
+            foreach (var dirName in directoryNames)
+            {
+                var dirPath = Path.Combine(basePath, dirName);
+                for (int i = 0; i < 10; i++)
+                {
+                    var fileName = "File" + (i + 1);
+                    var filePath = Path.Combine(dirPath, fileName);
+                    if (File.Exists(filePath))
+                    {
+                        var Text = File.ReadAllText(filePath);
+                        Console.WriteLine(Text);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ HomeWork11/Utils.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork11
+{
+    internal class Utils
+    {
+        public static string GetBasePath()
+        {
+            var basePath = "";
+            if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+            {
+                basePath = @"c:\Otus";
+            }
+            else
+            {
+                basePath = "/Otus";
+            }
+            return basePath;
+        }
+
+        public static bool CanWriteToFile(string filePath)
+        {
+            try
+            {
+                using (FileStream fs = File.OpenWrite(filePath))
+                {
+                    return true;
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                return false;
+            }
+            catch (IOException)
+            {
+                return false;
+            }
+        }
+    }
+}
